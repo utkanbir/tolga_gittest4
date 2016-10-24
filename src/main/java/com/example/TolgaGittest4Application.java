44539d0f234fd810acddf85fa7289c8908e2930d
Tolga Evren <tolgae@tani.com.tr>
2016-10-24 17:05:14 +0300

    aa

---
+++ src/main/java/com/example/TolgaGittest4Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolgaGittest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolgaGittest4Application.class, args);
+	}
+}
